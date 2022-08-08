2c30e8febb80074024d2e9570aa0c73b5052dade
Zeto24 <109522475+Zeto24@users.noreply.github.com>
2022-08-09 03:31:44 +0700

    Add files via upload

--- update.py
+++ update.py
@@ -7,12 +7,12 @@ def update():
     os.system("clear")
     print("\n","-"*32)
     print(" UPDATE/REINSTALL TOOLS BERHASIL!")
-    time.sleep(30)
+    time.sleep(0.015)
     os.system("cd aulia")
     os.system("python aulia.py")
     
-for i in range(10):
-    sys.stdout.write('•')
-    sys.stdout.flush()
-    time.sleep(1)
+#for i in range(10):
+#    sys.stdout.write('•')
+#    sys.stdout.flush()
+#    time.sleep(1)
 update()
