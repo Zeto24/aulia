fd60bc1b475351c63ed87cf146cba774484460a2
Zeto24 <109522475+Zeto24@users.noreply.github.com>
2022-08-09 03:20:22 +0700

    Add files via upload

--- update.py
+++ update.py
@@ -1,16 +1,18 @@
 import os
 import sys
 import time
+def update():
+    os.system("rm -rf aulia")
+    os.system("git clone https://github.com/Zeto24/aulia.git")
+    os.system("clear")
+    print("\n","-"*32)
+    print(" UPDATE/REINSTALL TOOLS BERHASIL!")
+    time.sleep(2)
+    os.system("cd aulia")
+    os.system("python aulia.py")
+    
 for i in range(10):
     sys.stdout.write('•')
     sys.stdout.flush()
     time.sleep(1)
-os.system("rm -rf aulia")
-os.system("git clone https://github.com/Zeto24/aulia.git")
-os.system("clear")
-os.system("clear")
-print("\n","-"*32)
-print(" UPDATE/REINSTALL TOOLS BERHASIL!")
-time.sleep(2)
-os.system("cd toolus")
-os.system("python aulia.py")
+update()
